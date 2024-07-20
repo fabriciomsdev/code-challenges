8fdace5fa3775b00727ba5e0127644f47a998c06
Fabricio Magalhães Sena <48806218+fabriciomsdev@users.noreply.github.com>
2024-07-20 17:46:44 -0300

    Create greatest-palindromic.ts

---
+++ greatest-palindromic.ts
@@ -0,0 +1,29 @@
+const revertString = (str: string): string => {
+    return str.split('').reverse().join('');
+}
+
+const isValid = (subString: string, greatestPalindromic: string): boolean => {
+    const isValid = subString.length > 1;
+    const isPalindromic = subString === revertString(subString);
+    const greatestLenght = greatestPalindromic?.length ?? 0;
+    const greatest =  subString.length > greatestLenght;
+
+    return isPalindromic && isValid && greatest;
+}
+
+const getGreatestPalindromic = (text: string): string => {
+    let greatestPalindromic = null;
+
+    for (let i = 0; i < text.length; i++) {
+        for (let j = i + 1; j < text.length; j++) {
+            const subString = text.slice(i, j);
+            if (subString && isValid(subString, greatestPalindromic)) greatestPalindromic = subString;
+        }
+    }
+    
+    return greatestPalindromic;
+}
+
+function longestPalindrome(s: string): string {
+    return getGreatestPalindromic(s);
+};
